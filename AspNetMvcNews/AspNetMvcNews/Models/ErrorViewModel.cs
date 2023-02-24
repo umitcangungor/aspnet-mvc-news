34b3b46946c7dd80ff7fd44ff62b48d648421648
worthowski <umitgungor98@gmail.com>
2023-02-24 21:35:28 +0300

    task 1 complited

---
+++ AspNetMvcNews/AspNetMvcNews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetMvcNews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
