e79ad2edd8b36ee5ad7492f61392180926fe94f7
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2023-02-25 21:22:18 +0300

    Task 1 Completed

---
+++ AspNetMvcNews/App.Web.Mvc/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/news/title-slug")]
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
