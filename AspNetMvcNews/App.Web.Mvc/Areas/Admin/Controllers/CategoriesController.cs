4e7932c99c0c7c45fcf0522f2915db9ac5a62a2d
MURAT CANKAT KÖK <mcankatkok@gmail.com>
2023-03-12 15:23:15 +0300

    Admin Category Views

--- AspNetMvcNews/App.Web.Mvc/Areas/Admin/Controllers/CategoriesController.cs
+++ AspNetMvcNews/App.Web.Mvc/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,13 +1,115 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Data.Entities;
+using App.Service.Abstract;
+using App.Web.Mvc.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace App.Web.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        private readonly IService<Category> _service;
+
+        public CategoriesController(IService<Category> service)
+        {
+            _service = service;
+        }
+
+        // GET: CategoriesController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: CategoriesController/Details/5
+        public ActionResult Details(int id)
         {
             return View();
+        }
+
+        // GET: CategoriesController/Create
+        public ActionResult Create()
+        {           
+            return View();        
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    await _service.AddAsync(category);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(category);
+        }
+
+        // GET: CategoriesController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: CategoriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Category category)
+        {
+            if (ModelState.IsValid) 
+            {
+                try
+                {
+                    
+                    _service.Update(category);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            
+            return View(category);
+        }
+
+        // GET: CategoriesController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: CategoriesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Category category)
+        {
+            try
+            {               
+                _service.Delete(category);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
