3e1c563f2e7df8df315ffbe02a0f60c3a920494c
Emirhan Yılmaz <emrannderin@gmail.com>
2023-03-21 22:59:55 +0300

    Admin Sayfası resim ekleme eksik kaldı

---
+++ AspNetMvcNews/App.Web.Mvc/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,119 @@
+﻿using App.Data.Entities;
+using App.Service.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace App.Web.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NewsController : Controller
+    {
+        private readonly IService<News> _service;
+        private readonly IService<Category> _serviceCategory;
+
+        public NewsController(IService<News> service, IService<Category> serviceCategory)
+        {
+            _service = service;
+            _serviceCategory = serviceCategory;
+        }
+
+        // GET: NewsController
+
+        public async Task<ActionResult> IndexAsync()
+        {
+
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: NewsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewsController/Create
+        public async Task<ActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _serviceCategory.GetAllAsync(), "Id", "Name");
+            
+            return View();
+        }
+
+
+        // POST: NewsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(News news)
+        {
+            try
+            {
+                await _service.AddAsync(news);
+                await _service.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu!");
+            }
+            ViewBag.CategoryId = new SelectList(await _serviceCategory.GetAllAsync(), "Id", "Name");
+            return View(news);
+        }
+
+        // GET: NewsController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            ViewBag.CategoryId = new SelectList(await _serviceCategory.GetAllAsync(), "Id", "Name");
+            return View(model);
+        }
+
+        // POST: NewsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(News news, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    _service.Update(news);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            ViewBag.CategoryId = new SelectList(await _serviceCategory.GetAllAsync(), "Id", "Name");
+            return View(news);
+        }
+
+        // GET: NewsController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: NewsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(News news, int id)
+        {
+            try
+            {
+                _service.Delete(news);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AspNetMvcNews/App.Web.Mvc/Areas/Admin/Controllers/NewsImageController.cs
@@ -0,0 +1,95 @@
+﻿using App.Data.Entities;
+using App.Service.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class NewsImageController : Controller
+    {
+        private readonly IService<News> _service;
+
+
+        public NewsImageController(IService<News> service)
+        {
+            _service = service;
+        }
+        // GET: NewsImageController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: NewsImageController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewsImageController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewsImageController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewsImageController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NewsImageController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewsImageController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NewsImageController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
