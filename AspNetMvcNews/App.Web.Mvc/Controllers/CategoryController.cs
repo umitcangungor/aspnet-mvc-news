e414d7a0eb00e93545e06ed6e317a9352cbeeb86
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2023-04-01 23:31:53 +0300

    news carousel düzelttim

--- AspNetMvcNews/App.Web.Mvc/Controllers/CategoryController.cs
+++ AspNetMvcNews/App.Web.Mvc/Controllers/CategoryController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Service.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace App.Web.Mvc.Controllers
 {
 	public class CategoryController : Controller
 	{
-		[Route("/category/category-slug")]
-		public IActionResult Index()
+		private readonly ICategoryService _service;
+
+		public CategoryController(ICategoryService service)
 		{
-			return View();
+			_service = service;
+		}
+		[Route("/category/category-slug")]
+		public async Task<IActionResult> Index(int id)
+		{
+			var model = await _service.GetCategoryByNews(id);
+			return View(model);
 		}
 	}
 }
