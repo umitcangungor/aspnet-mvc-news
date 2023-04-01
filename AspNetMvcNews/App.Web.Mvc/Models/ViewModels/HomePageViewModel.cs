ea2f051eecb87d277d6d465a571b8c3a56cc44b6
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2023-04-02 01:00:42 +0300

    Kategori, Kategori Detail, Admin Paneli Kategori ve Haber işlemleri yapıldı.

---
+++ AspNetMvcNews/App.Web.Mvc/Models/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using App.Data.Entities;
+using System.Drawing.Drawing2D;
+
+namespace App.Web.Mvc.Models.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public List<News>? NewsList { get; set; }
+    }
+}
