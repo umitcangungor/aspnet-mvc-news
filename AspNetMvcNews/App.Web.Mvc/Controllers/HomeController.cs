f0594efe0da46850418a00cac25b7a351fc100a6
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2023-04-02 13:53:10 +0300

    Anasayfa işlemleri yapıldı

--- AspNetMvcNews/App.Web.Mvc/Controllers/HomeController.cs
+++ AspNetMvcNews/App.Web.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using App.Web.Mvc.Models;
+﻿using App.Data.Entities;
+using App.Service.Abstract;
+using App.Web.Mvc.Models;
+using App.Web.Mvc.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +10,24 @@ namespace App.Web.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IService<News> _service;
+        private readonly IService<Category> _categoryService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IService<News> service, IService<Category> categoryService)
         {
             _logger = logger;
+            _service = service;
+            _categoryService = categoryService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            var model = new HomePageViewModel()
+            {
+                NewsList = await _service.GetAllAsync(p => p.IsHome),
+                Categories = await _categoryService.GetAllAsync()
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
--- AspNetMvcNews/App.Web.Mvc/Models/ViewModels/HomePageViewModel.cs
+++ AspNetMvcNews/App.Web.Mvc/Models/ViewModels/HomePageViewModel.cs
@@ -5,5 +5,6 @@ namespace App.Web.Mvc.Models.ViewModels
 	public class HomePageViewModel
     {
         public List<News>? NewsList { get; set; }
+        public List<Category>? Categories { get; set; }
     }
 }
