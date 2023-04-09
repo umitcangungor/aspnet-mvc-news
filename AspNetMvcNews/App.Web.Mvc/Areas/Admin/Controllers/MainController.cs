fb3598970edd4e5ccb88cc98e306fbe0612eaf20
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2023-04-09 18:30:51 +0300

    Admin logout eklendi.

--- AspNetMvcNews/App.Web.Mvc/Areas/Admin/Controllers/MainController.cs
+++ AspNetMvcNews/App.Web.Mvc/Areas/Admin/Controllers/MainController.cs
@@ -1,7 +1,9 @@
-﻿using App.Data.Entities;
+﻿using App.Data;
+using App.Data.Entities;
 using App.Service.Abstract;
 using App.Web.Mvc.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Web.Mvc.Areas.Admin.Controllers
@@ -11,15 +13,17 @@ namespace App.Web.Mvc.Areas.Admin.Controllers
 	{
 		private readonly IService<News> _service;
 		private readonly IService<Category> _categoryService;
+		private readonly SignInManager<AppUser> _signInManager;
 
-		public MainController(IService<News> service, IService<Category> categoryService)
+		public MainController(IService<News> service, IService<Category> categoryService, SignInManager<AppUser> signInManager)
 		{
 			_service = service;
 			_categoryService = categoryService;
+			_signInManager = signInManager;
 		}
 
 
-		public async Task<IActionResult> IndexAsync()
+		public async Task<IActionResult> Index()
 		{
 			var model = new HomePageViewModel()
 			{
@@ -27,6 +31,14 @@ namespace App.Web.Mvc.Areas.Admin.Controllers
 				Categories = await _categoryService.GetAllAsync(),
 			};
 			return View(model);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> LogOut()
+		{
+			await _signInManager.SignOutAsync();
+			return Redirect("~/");
 		}
 	}
 }
